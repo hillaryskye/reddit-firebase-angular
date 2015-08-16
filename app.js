825027de1afc16f9661693232fcdc418c6c4d6b1
Hillary Skye <skyeinc@qwestoffice.net>
2015-08-16 14:08:37 -0600

    got most of it done

--- app.js
+++ app.js
@@ -1,82 +1,111 @@
-var app = angular.module("redditApp", []);
-
-app.controller('reddit', function($scope) {
-  $scope.votes = 0;
-  $scope.add = false;
-  $scope.comments = [];
+var app = angular.module("redditApp", ['angularMoment']);
+  app.controller("reddit", function($scope) {
+    $scope.add = false;
+    $scope.addComment = false;
+    $scope.addSearch = false;
+    // $scope.comments = [];
      $scope.posts = [
        {
          name: 'Aspen',
          url: 'http://s11.postimg.org/6lx1zbzqr/maroon_bells.jpg',
          desc: 'Lorem ipsum dolor sit amet, id dolor, vel scelerisque odio vestibulum volutpat, ornare vestibulum congue. Adipiscing non hendrerit vitae adipiscing ut vestibulum, metus urna. Tempor vitae sodales sem dapibus duis, ut ligula cras phasellus id, in vitae. Laoreet tortor consectetuer, arcu ut aliquid eget sit fringilla. Sed arcu, eget turpis mollis, pede at in wisi. Rhoncus feugiat, sit auctor urna nunc ac non, eros justo ullamcorper aliquam interdum ipsum mollis, sit nulla id aliquam fermentum, nunc vitae amet vivamus. Quam et ipsum eget ac adipiscing id, sed pellentesque pede lacinia.',
          author: 'Hillary Skye',
-        //  comment: 'This is the best place to camp!'
+         date: Date.now(),
+         votes: 2,
+         commentsArr : ["hello"]
        },
        {
         name: 'Yosemite',
         url: 'http://s11.postimg.org/ccn8d27qr/yosemite.jpg',
         desc: 'Lorem ipsum dolor sit amet, id dolor, vel scelerisque odio vestibulum volutpat, ornare vestibulum congue. Adipiscing non hendrerit vitae adipiscing ut vestibulum, metus urna. Tempor vitae sodales sem dapibus duis, ut ligula cras phasellus id, in vitae. Laoreet tortor consectetuer, arcu ut aliquid eget sit fringilla. Sed arcu, eget turpis mollis, pede at in wisi. Rhoncus feugiat, sit auctor urna nunc ac non, eros justo ullamcorper aliquam interdum ipsum mollis, sit nulla id aliquam fermentum, nunc vitae amet vivamus. Quam et ipsum eget ac adipiscing id, sed pellentesque pede lacinia.',
         author: 'Leya Rowen Arnold',
-        date: '12 Aug 2015'
+        date: Date.now(),
+        votes: 7,
+        commentsArr : ["hello post 22", "other comment??"]
       },
       {
         name: 'Cliffs of Mohr',
         url: 'http://s11.postimg.org/iz9w68pf7/cliffs_mohr.jpg',
         desc: 'Lorem ipsum dolor sit amet, id dolor, vel scelerisque odio vestibulum volutpat, ornare vestibulum congue. Adipiscing non hendrerit vitae adipiscing ut vestibulum, metus urna. Tempor vitae sodales sem dapibus duis, ut ligula cras phasellus id, in vitae. Laoreet tortor consectetuer, arcu ut aliquid eget sit fringilla. Sed arcu, eget turpis mollis, pede at in wisi. Rhoncus feugiat, sit auctor urna nunc ac non, eros justo ullamcorper aliquam interdum ipsum mollis, sit nulla id aliquam fermentum, nunc vitae amet vivamus. Quam et ipsum eget ac adipiscing id, sed pellentesque pede lacinia.',
         author: 'Dylan Arnold',
-        date: 'Today, 15 Aug 2015'
+        date: Date.now(),
+        votes: 40,
+        commentsArr : ["hello post 3", "other comment??"]
       },
       {
         name: 'Yellowstone',
         url: 'http://s11.postimg.org/6zydyxjub/yellowstone.jpg',
         desc: 'Lorem ipsum dolor sit amet, id dolor, vel scelerisque odio vestibulum volutpat, ornare vestibulum congue. Adipiscing non hendrerit vitae adipiscing ut vestibulum, metus urna. Tempor vitae sodales sem dapibus duis, ut ligula cras phasellus id, in vitae. Laoreet tortor consectetuer, arcu ut aliquid eget sit fringilla. Sed arcu, eget turpis mollis, pede at in wisi. Rhoncus feugiat, sit auctor urna nunc ac non, eros justo ullamcorper aliquam interdum ipsum mollis, sit nulla id aliquam fermentum, nunc vitae amet vivamus. Quam et ipsum eget ac adipiscing id, sed pellentesque pede lacinia.',
         author: 'Dylan Arnold',
-        date: '15 July 2015'
+        date: Date.now(),
+        votes: -2,
+        commentsArr : ["hello post 3", "other comment??"]
       }
      ]
 
-     $scope.addDate = function() {
-       $scope.date = Date.now()
-       var date = new Date()
+    //  $scope.addDate = function(todaysDate) {
+    //    $scope.todaysDate = Date.now()
+    //    var date = new Date()
+    //    console.log('date', $scope.todaysDate)
+    //  }
+     $scope.addPost = function(post) {
        console.log('date', $scope.date)
-     }
-     $scope.addPost = function () {
-       console.log('add', $scope.add)
        console.log('hello');
-       console.log({name: $scope.name, url: $scope.url, desc: $scope.desc, author: $scope.author, comments: $scope.comment});
-       $scope.posts.push(
-         {
+       console.log({name: $scope.name, url: $scope.url, desc: $scope.desc, author: $scope.author});
+         $scope.post = {
            name: $scope.name,
            url: $scope.url,
            desc: $scope.desc,
            author: $scope.author,
-           comments: [$scope.comment],
-           date: $scope.date,
-           votes: $scope.votes
-         })
-         $scope.add = false;
+           date: Date.now(),		// we could skip all of this creating the 'post' object, if we didn't need to add the current time to it
+           votes: 0,
+           commentsArr: []
+         }
+       $scope.posts.push($scope.post)
+      //  $scope.post = null;
+       $scope.add = false;
      }
-     $scope.addVotes = function() {
-       console.log('votes')
-       $scope.votes++
+     $scope.addVotes = function(post) {
+       post.votes++
      }
-     $scope.delVotes = function() {
+     $scope.delVotes = function(post) {
        console.log('del votes')
-       $scope.votes--
+       post.votes--
      }
-     $scope.addComments = function($scope, $index) {
+
+     $scope.addComments = function(post, $index) {
+       console.log('num of comments', $scope.commentsArr[$index]);
+        console.log('num', $index);
        $scope.posts.push(
          {
-           comments: $scope.posts.comments,
+           comments: $scope.posts.commentsArr,
          })
      }
-     console.log('add', $scope.add)
+     console.log('addComment', $scope.addComment)
 
      $scope.addForm = function() {
        if ($scope.add === false) {
          $scope.add = true;
        } else if ($scope.add === true) {
          $scope.add = false;
+       }
+     }
+     $scope.addCommentsForm = function() {
+       console.log('comments', $scope.addComment);
+       if ($scope.addComment === false) {
+         $scope.addComment = true;
+       } else if ($scope.addComment === true) {
+         $scope.addComment = false;
+       }
+     }
+     $scope.addSearchSelect = function() {
+       console.log('search', $scope.addSearch);
+       if ($scope.addSearch === false) {
+         $scope.addSearch = true;
+         console.log('search', $scope.addSearch);
+
+       } else if ($scope.addSearch === true) {
+         $scope.addSearch = false;
        }
      }
    })
